40cd441f8d3d150a6ef105bd1633f8440dd988b9
TinySpins <100190778+TinySpins@users.noreply.github.com>
2023-05-23 10:44:58 +0200

    updated

--- moddcm/moddcm.sh
+++ moddcm/moddcm.sh
@@ -3,7 +3,7 @@
 # Input file parent folder name as PatientName (0010,0010)
 find . -name '*.dcm' -exec sh -c 'dcmodify -i "(0010,0010)=${0%/*}" "{}"' {} \;
 
-# Input filename as SeriesDescription (0008,103e)
+# Input file name as SeriesDescription (0008,103e)
 find . -name '*.dcm' -exec sh -c 'dcmodify -i "(0008,103e)=${0##*/}" "{}"' {} \;
 
 # Input main directory as StudyDescription (0008,1030)
